23c67d50a74b42cc9528530c87acfde3a5b0df0d
alhoffiq <andrew.hoff14@yahoo.com>
2021-08-31 20:23:00 -0700

    adjusted button logic

--- src/App.js
+++ src/App.js
@@ -3,6 +3,33 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form'
 
 function App() {
+  let evidenceCount = 0;
+
+  function addEvidence(type) {
+    console.log(type);
+    console.log(evidenceCount);
+    switch (evidenceCount) {
+      case 0:
+        evidenceCount++
+        break;
+
+      case 1:
+        evidenceCount++
+        break;
+
+      case 2:
+        evidenceCount++
+        break;
+
+      case 3:
+
+        break;
+
+      default:
+        break;
+    }
+  }
+
   return (
     <div className="container">
       <div className="row">
@@ -16,35 +43,37 @@ function App() {
           <Form.Control type="text" placeholder="Evidence 3" readOnly />
         </div>
         <div className="col-6">
-          <Form.Control type="text" placeholder="Ghost type" readOnly />
+          <Form.Control type="text" placeholder="Ghost type" readOnly  />
         </div>
       </div>
       <div className="row">
         <div className="col-6">
           <div className="row">
             <div className="col-4">
-              <Button variant="outline-primary" data="emf">Emf level 5</Button>
+              <Button variant="outline-primary" onClick={() => addEvidence("emf")}>Emf level 5</Button>
             </div>
             <div className="col-4">
-              <Button variant="outline-primary" data="finger">Fingerprints</Button>
+              <Button variant="outline-primary" onClick={() => addEvidence("finger")}>Fingerprints</Button>
             </div>
             <div className="col-4">
-              <Button variant="outline-primary" data="orb">Ghost orbs</Button>
+              <Button variant="outline-primary" onClick={() => addEvidence("orb")}>Ghost orbs</Button>
             </div>
             <div className="col-4">
-              <Button variant="outline-primary" data="box">Spirit box</Button>
+              <Button variant="outline-primary" onClick={() => addEvidence("box")}>Spirit box</Button>
             </div>
             <div className="col-4">
-              <Button variant="outline-primary" data="writing">Ghost writing</Button>
+              <Button variant="outline-primary" onClick={() => addEvidence("writing")}>Ghost writing</Button>
             </div>
             <div className="col-4">
-              <Button variant="outline-primary" data="temps">Freezing temperatures</Button>
+              <Button variant="outline-primary" onClick={() => addEvidence("temps")}>Freezing temperatures</Button>
             </div>
             <div className="col-4"></div>
             <div className="col-4">
-              <Button variant="outline-primary" data="dots">D.O.T.S projector</Button>
+              <Button variant="outline-primary" onClick={() => addEvidence("dots")}>D.O.T.S projector</Button>
             </div>
-            <div className="col-4"></div>
+            <div className="col-4">
+              <Button variant="outline-primary" onClick={() => addEvidence("dots")}>Reset</Button>
+            </div>
           </div>
         </div>
         <div className="col-6">
